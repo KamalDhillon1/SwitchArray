b482e8e67e425548cce1ba7860dc8aeac3110e1b
KamalDhillon <kkaur7887@gmail.com>
2022-01-24 12:54:22 -0800

    Switch and Array

---
+++ src/Examples/ArrayPractice.java
@@ -0,0 +1,18 @@
+package Examples;
+
+public class ArrayPractice {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+		// 1st way of declaring array
+		String[] food = { "mexican", "italian", "indian", "american" };
+		// 2nd way of declaring array
+		String[] food1 = new String[4];
+
+		// Access value
+		System.out.println(food[2]);
+		System.out.println(food[3]);
+	}
+
+}
---
+++ src/Examples/SwitchPractice.java
@@ -0,0 +1,29 @@
+package Examples;
+
+public class SwitchPractice {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+		// We have 1 machine and 3 buttons on that coffee machine ,each button gives
+		// different coffee
+		// Button 1-Black tea
+		// Button 2-Orange pekoe
+		// Button 3-hot chocolate
+		int button = 2;
+		switch (button) {
+		case 1:
+			// Button1
+			System.out.println("Pour Black tea");
+			break;
+		case 2:
+			// Button2
+			System.out.println("Pour orange pekoe");
+			break;
+		case 3:
+			// Button3
+			System.out.println("Pour hot chocolate");
+		}
+				}
+
+}
